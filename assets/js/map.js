c5c40e71601a7cb2a4810c991032d55856829c08
akhil18mittal <akhil18.mittal@gmail.com>
2025-04-20 21:38:59 +0530

    Add Google Maps integration and update footer with contact form

---
+++ assets/js/map.js
@@ -0,0 +1,165 @@
+function initMap() {
+  // The location of your business
+  const location = { lat: 28.7041, lng: 77.1025 }; // Example coordinates for Delhi
+  // The map, centered at your location
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: location,
+    styles: [
+      {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "color": "#000000"
+          },
+          {
+            "weight": 2
+          }
+        ]
+      },
+      {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#144b53"
+          },
+          {
+            "weight": 1.4
+          }
+        ]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#08304b"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#0c4152"
+          },
+          {
+            "lightness": 5
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#0b434f"
+          },
+          {
+            "lightness": 25
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#0b3d51"
+          },
+          {
+            "lightness": 16
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#146474"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#021019"
+          }
+        ]
+      }
+    ]
+  });
+  // The marker, positioned at your location
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: "Ashok Wires & Chemicals",
+    icon: {
+      url: "{{ '/assets/images/marker.png' | relative_url }}",
+      scaledSize: new google.maps.Size(50, 50)
+    }
+  });
+} 
