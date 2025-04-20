7beaec26ef7ac09218518b71ac35d3d8ba507ccd
akhil18mittal <akhil18.mittal@gmail.com>
2025-04-20 22:01:13 +0530

    Improve carousel initialization and script loading order

--- assets/js/script.js
+++ assets/js/script.js
@@ -127,51 +127,57 @@ jQuery(function ($) {
 
 		// banner-carousel
 		function bannerCarouselOne() {
-			$('.banner-carousel.banner-carousel-1').slick({
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				autoplay: true,
-				dots: true,
-				speed: 600,
-				arrows: true,
-				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
-				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
-			});
-			$('.banner-carousel.banner-carousel-1').slickAnimation();
+			if ($('.banner-carousel.banner-carousel-1').length) {
+				$('.banner-carousel.banner-carousel-1').slick({
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					autoplay: true,
+					dots: true,
+					speed: 600,
+					arrows: true,
+					prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+					nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+				});
+				$('.banner-carousel.banner-carousel-1').slickAnimation();
+			}
 		}
 		bannerCarouselOne();
 
 
 		// banner Carousel Two
 		function bannerCarouselTwo() {
-			$('.banner-carousel.banner-carousel-2').slick({
-				fade: true,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				autoplay: true,
-				dots: false,
-				speed: 600,
-				arrows: true,
-				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
-				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
-			});
+			if ($('.banner-carousel.banner-carousel-2').length) {
+				$('.banner-carousel.banner-carousel-2').slick({
+					fade: true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					autoplay: true,
+					dots: false,
+					speed: 600,
+					arrows: true,
+					prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+					nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+				});
+			}
 		}
 		bannerCarouselTwo();
 
 
 		// pageSlider
 		function pageSlider() {
-			$('.page-slider').slick({
-				fade: true,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				autoplay: true,
-				dots: false,
-				speed: 600,
-				arrows: true,
-				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
-				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
-			});
+			if ($('.page-slider').length) {
+				$('.page-slider').slick({
+					fade: true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					autoplay: true,
+					dots: false,
+					speed: 600,
+					arrows: true,
+					prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+					nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+				});
+			}
 		}
 		pageSlider();
 
